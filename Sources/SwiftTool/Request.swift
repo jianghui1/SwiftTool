04aca0ed1775038a6f77e4a1291834510a5b86ee
jzh <jiangzenghui005@163.com>
2023-07-28 15:37:52 +0800

    修改请求返回数据类型。

--- Sources/SwiftTool/Request.swift
+++ Sources/SwiftTool/Request.swift
@@ -10,13 +10,12 @@ import Foundation
 // 使用 async 关键字声明异步函数
 public func fetchData(from urlString: String) async throws -> (Data, URLResponse) {
     if let url = URL(string: urlString) {
-        let result = try await URLSession.shared.data(from: url)
-        return result
+        return try await URLSession.shared.data(from: url)
     }
     throw URLError(.badURL)
 }
 
-public func fetchData(from urlString: String, method: String, body: [String: Any]) async throws -> Data {
+public func fetchData(from urlString: String, method: String, body: [String: Any]) async throws -> (Data, URLResponse) {
     // 将字典转换为Data类型
     guard let postData = try? JSONSerialization.data(withJSONObject: body, options: []) else {
         fatalError("请求body错误")
@@ -26,8 +25,7 @@ public func fetchData(from urlString: String, method: String, body: [String: Any
         request.httpMethod = method
         request.httpBody = postData
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        let (data, _) = try await URLSession.shared.data(for: request)
-        return data
+        return try await URLSession.shared.data(for: request)
     }
     else {
         throw URLError(.badURL)
